9277538eaec92500557cd037101c2cb5f99c8626
Hunobas <po127992@gmail.com>
2021-12-21 14:41:31 +0900

    ~6.1 Marshal and Field Tags

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+	Foo         string `json:"-"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         "/block",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+			Foo:         "damn",
+		},
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+	json.NewEncoder(rw).Encode(data)
+}
+
+func main() {
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
