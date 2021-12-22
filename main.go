e65419ed217566417bb0ebe829de1aac6798abdb
Hunobas <po127992@gmail.com>
2021-12-22 15:33:36 +0900

    buildRestAPI (~6.4 NewServeMux)

--- main.go
+++ main.go
@@ -1,81 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/Hunobas/nomadcoin/blockchain"
-	"github.com/Hunobas/nomadcoin/utils"
+	"github.com/Hunobas/nomadcoin/explorer"
+	"github.com/Hunobas/nomadcoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-type AddBlockBody struct {
-	Message string
-}
-
-// func (u URL) String() string {
-// 	return "hi"
-// }
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks/{id}"),
-			Method:      "GET",
-			Description: "See A Block",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-
-	rw.Header().Add("Content-Type", "application/json")
-	// b, err := json.Marshal(data)
-	// utils.HandleErr(err)
-	// fmt.Fprintf(rw, "%s", b)
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	go explorer.Start(4000)
+	rest.Start(4000)
 }
