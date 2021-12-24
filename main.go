499e26144c818dc74221884a2ad51f0f863cec06
Hunobas <po127992@gmail.com>
2021-12-24 20:30:13 +0900

    setCLI (~7.2 FlagSet)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -18,12 +19,20 @@ func main() {
 		usage()
 	}
 
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Set the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer...")
 	case "rest":
-		fmt.Println("Start REST API...")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(portFlag)
+		fmt.Printf("Start Server with the port %d\n", *portFlag)
 	}
 }
