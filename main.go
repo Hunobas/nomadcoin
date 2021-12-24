ebc9f78c7e9687252d96ad72a6594c68ea7d0359
Hunobas <po127992@gmail.com>
2021-12-24 20:06:04 +0900

    setCLI (~7.1 Parsing Commands)

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"github.com/Hunobas/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("!!Welcome to 노마드 코인!!\n\n")
+	fmt.Printf("\tPlease use the following commands:\n\n")
+	fmt.Printf("\texplorer:	Start the HTML Explorer\n")
+	fmt.Printf("\trest:		Start the REST API (recommanded)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer...")
+	case "rest":
+		fmt.Println("Start REST API...")
+	default:
+		usage()
+	}
 }
