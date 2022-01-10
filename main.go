66dea34fd927a801394f263c6919040d127f396c
Hunobas <po127992@gmail.com>
2022-01-10 21:05:16 +0900

    Mining3 (~9.3 Difficulty part One)

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/Hunobas/nomadcoin/cli"
+	"github.com/Hunobas/nomadcoin/db"
 )
 
 func main() {
-	difficulty := 6
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		if strings.HasPrefix(hash, target) {
-			fmt.Printf("hello%d\n", nonce)
-			fmt.Println(hash)
-			return
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
