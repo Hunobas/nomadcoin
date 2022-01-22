08be71861fa2bf46fbebd3e18787497f2af588b9
Hunobas <po127992@gmail.com>
2022-01-22 21:34:01 +0900

    P2P3 (~12.3 Read, Recieve and Close)

--- main.go
+++ main.go
@@ -5,12 +5,26 @@ import (
 	"time"
 )
 
-func countToTen(c chan int) {
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
 		time.Sleep(1 * time.Second)
 		fmt.Printf("sending %d\n", i)
 		c <- i
 	}
+	close(c)
+}
+
+func receive(c <-chan int) []int {
+	var a []int
+	for {
+		ac, ok := <-c
+		if !ok {
+			fmt.Println("we are done.")
+			break
+		}
+		a = append(a, ac)
+	}
+	return a
 }
 
 func main() {
@@ -18,10 +32,7 @@ func main() {
 	go countToTen(c)
 	// a := <-c			 // <-c == for{} == Program is waiting
 	fmt.Println("blocking...")
-	var a []int
-	for {
-		a = append(a, <-c)
-	}
+	a := receive(c)
 	fmt.Printf("received %d\n", a)
 	fmt.Println(a)
 }
