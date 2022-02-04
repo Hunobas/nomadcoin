91f6ae55c1a1c0b17110efa387be81759b71b1c6
Hunobas <po127992@gmail.com>
2022-02-05 03:06:18 +0900

    p2p9 (~12.9 Peers)

--- rest/rest.go
+++ rest/rest.go
@@ -45,6 +45,10 @@ type addTxPayload struct {
 	Amount int
 }
 
+type addPeerPayload struct {
+	address, port string
+}
+
 func (u url) MarshalText() ([]byte, error) {
 	url := fmt.Sprintf("http://localhost%s%s", port, u)
 	return []byte(url), nil
@@ -171,6 +175,16 @@ func myWallet(rw http.ResponseWriter, r *http.Request) {
 	}{Address: address}) // anonymous struct
 }
 
+func peers(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var payload addPeerPayload
+		json.NewDecoder(r.Body).Decode(&payload)
+		p2p.AddPeer(payload.address, payload.port)
+		rw.WriteHeader(http.StatusOK)
+	}
+}
+
 func Start(aPort int) {
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
@@ -185,6 +199,7 @@ func Start(aPort int) {
 	router.HandleFunc("/wallet", myWallet).Methods("GET")
 	router.HandleFunc("/transactions", transactions).Methods("POST")
 	router.HandleFunc("/ws", p2p.Upgrade).Methods("GET")
+	router.HandleFunc("/peers", peers).Methods("POST")
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
